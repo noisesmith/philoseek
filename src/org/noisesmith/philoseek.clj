d2ccdf31fabf3e3b568f82a3b9903d609a298ca1
Justin Glenn Smith <noisesmith@gmail.com>
2017-11-29 14:06:12 -0800

    function to help us know how we will find this data

--- src/org/noisesmith/philoseek.clj
+++ src/org/noisesmith/philoseek.clj
@@ -2,6 +2,10 @@
   (:require [org.noisesmith.poirot :as p]
             [clojure.xml :as x]))
 
+(defn find-tag
+  [& tags]
+  (filter (comp (set tags) :tag)))
+
 (defn extract-link
   "finds the next link to follow from the body"
   [body]
@@ -15,3 +19,24 @@
        (x/parse)
        #_ (doto (p/dump "parsed-wiki-data"))
        (extract-link))))
+
+
+(def basic-dump-file "test/data/parsed-wiki-data.transit.json")
+
+(defn explore-links
+  "function that exists for exploring our data in the repl"
+   [n]
+    (-> (p/restore basic-dump-file)
+        (->> (tree-seq coll? seq)
+             (sequence (find-tag :a)))
+        (nth n)))
+
+;; not the data we want: (explore-links 0) (explore-links 1) etc.
+
+;; probably what we want? (explore-links 4)
+
+;; why tree-seq: "I don't know much about web pages, web devs are always
+;; doing all kinds of crazy stuff, but I do know that if I randomly jumble
+;; through the data I'll probably find something with the properties that
+;; let me know it's the thing I want, tree-seq is perfect for this kind of
+;; ignorant searching"
--- test/org/noisesmith/philoseek_test.clj
+++ test/org/noisesmith/philoseek_test.clj
@@ -3,8 +3,6 @@
             [org.noisesmith.poirot :as p]
             [org.noisesmith.philoseek :refer :all]))
 
-(def basic-dump-file "test/data/parsed-wiki-data.transit.json")
-
 (deftest search-test
   (testing "basic functionality"
     (is (string? (extract-link (p/restore basic-dump-file))))))
